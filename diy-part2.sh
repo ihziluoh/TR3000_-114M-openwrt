e6ed77afce8f8ef09e524c19026f9b949ffdb6de
ihziluoh <ihziluoh@users.noreply.github.com>
2025-04-03 19:42:10 +0800

    修改主机名称
    修改主机名称为Cudy_TR3000

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,10 +14,10 @@
 sed -i '/label = "ubi"/{n;s/reg = <0x5c0000 0x[0-9a-f]\+>/reg = <0x5c0000 0x7200000>/}' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1.dts 
 
 # 修改默认ip地址 
-sed -i 's/192.168.16.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.16.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 修改主机名称
+sed -i 's/OpenWrt/Cudy-TR3000/g' package/base-files/files/bin/config_generate
