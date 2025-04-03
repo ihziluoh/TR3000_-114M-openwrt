494a9d2df1c12b264048d3ea8e2d8c4da64fe41d
ihziluoh <ihziluoh@users.noreply.github.com>
2025-04-03 19:54:03 +0800

    修改时区
    修改时区 为上海

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,3 +21,6 @@ sed -i 's/192.168.1.1/192.168.16.1/g' package/base-files/files/bin/config_genera
 
 # 修改主机名称
 sed -i 's/OpenWrt/Cudy-TR3000/g' package/base-files/files/bin/config_generate
+
+# 修改时区
+sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
